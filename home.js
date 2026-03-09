21066b50fb4e2ae7beb4a688651d01030138666e
Reyad-Hoq <rafiulhoq40@gmail.com>
2026-03-09 23:54:08 +0600

    open and closed tab functionally work

--- home.js
+++ home.js
@@ -7,36 +7,187 @@ const loadIssues = async () => {
   const data = await res.json();
   renderAllIssues(data.data)
   renderOpenIssues(data.data)
-  const openIssues = data.data.filter(issue => issue.status === "open");
-  const openContainer = document.getElementById("open-tab");
-
-  openIssues.forEach(issue => {
-
-    const div = document.createElement("div");
-
-    div.innerHTML = `
-        <div class="card">
-          <h3>${issue.title}</h3>
-          <h3>${issue.description}</h3>
-          <h3>${issue.priority}</h3>
-          <p>${issue.status}</p>
-        </div>
-      `;
-
-    openContainer.appendChild(div);
-
-  });
+  renderClosedIssues(data.data)
 }
 
 loadIssues();
-const renderOpenIssues = (issues) => {
-  console.log(issues)
+const renderClosedIssues = (issues) => {
+
   const openContainer = document.getElementById("close-container");
   openContainer.innerHTML = "";
-  const openItem = issues.filter(item => {
-    item.status === "open"
-  })
-  console.log(openItem)
+
+  const openItems = issues.filter(issue => issue.status === "closed");
+
+  openItems.forEach(issue => {
+
+    const statusIcon = issue.status === "open"
+      ? "./assets/Open-Status.png"
+      : "./assets/Closed- Status .png";
+
+    const priorityColor = issue.priority === "low"
+      ? "text-green-500"
+      : issue.priority === "medium"
+        ? "text-yellow-400"
+        : "text-red-400";
+
+    const statusColor = issue.status === "open"
+      ? "border-t-3 border-success"
+      : "border-t-3 border-error";
+
+    const labelsHTML = issue.labels.map(label => {
+      let color = "btn-soft";
+      let icon = "";
+
+      if (label === "bug") {
+        color = "btn-error";
+        icon = `<i class="fa-solid fa-bug"></i>`;
+      }
+      if (label === "help wanted") {
+        color = "btn-warning";
+        icon = `<i class="fa-solid fa-life-ring"></i>`;
+      }
+      if (label === "enhancement") {
+        color = "btn-success";
+        icon = `<i class="fa-solid fa-wand-magic-sparkles"></i>`;
+      }
+      if (label === "documentation") {
+        color = "btn-info";
+        icon = `<i class="fa-regular fa-file"></i>`;
+      }
+      if (label === "good first issue") {
+        color = "btn-primary";
+        icon = `<i class="fa-solid fa-thumbs-up"></i>`;
+      }
+
+      return `
+        <button class="btn btn-soft ${color} rounded-full">
+          <span>${icon}</span> ${label}
+        </button>
+      `;
+    }).join("");
+
+    const card = document.createElement("div");
+
+    card.innerHTML = `
+      <div class="bg-white rounded-lg shadow-lg space-y-4 p-4 ${statusColor}">
+          <div class="flex justify-between">
+            <div>
+              <img src="${statusIcon}" alt="">
+            </div>
+            <p class="text-[12px] font-bold ${priorityColor}">
+              ${issue.priority.toUpperCase()}
+            </p>
+          </div>
+
+          <div class="space-y-3">
+            <h2 class="text-sm font-semibold">${issue.title}</h2>
+            <p class="text-gray-500 text-[12px]">${issue.description}</p>
+
+            <div>
+              ${labelsHTML}
+            </div>
+          </div>
+
+          <div class="text-gray-500 text-[12px]">
+            <p>#${issue.id} by ${issue.author}</p>
+            <p>${issue.createdAt}</p>
+          </div>
+        </div>
+    `;
+
+    openContainer.appendChild(card);
+
+  });
+
+}
+const renderOpenIssues = (issues) => {
+
+  const openContainer = document.getElementById("open-container");
+  openContainer.innerHTML = "";
+
+  const openItems = issues.filter(issue => issue.status === "open");
+
+  openItems.forEach(issue => {
+
+    const statusIcon = issue.status === "open"
+      ? "./assets/Open-Status.png"
+      : "./assets/Closed-Status.png";
+
+    const priorityColor = issue.priority === "low"
+      ? "text-green-500"
+      : issue.priority === "medium"
+        ? "text-yellow-400"
+        : "text-red-400";
+
+    const statusColor = issue.status === "open"
+      ? "border-t-3 border-success"
+      : "border-t-3 border-error";
+
+    const labelsHTML = issue.labels.map(label => {
+      let color = "btn-soft";
+      let icon = "";
+
+      if (label === "bug") {
+        color = "btn-error";
+        icon = `<i class="fa-solid fa-bug"></i>`;
+      }
+      if (label === "help wanted") {
+        color = "btn-warning";
+        icon = `<i class="fa-solid fa-life-ring"></i>`;
+      }
+      if (label === "enhancement") {
+        color = "btn-success";
+        icon = `<i class="fa-solid fa-wand-magic-sparkles"></i>`;
+      }
+      if (label === "documentation") {
+        color = "btn-info";
+        icon = `<i class="fa-regular fa-file"></i>`;
+      }
+      if (label === "good first issue") {
+        color = "btn-primary";
+        icon = `<i class="fa-solid fa-thumbs-up"></i>`;
+      }
+
+      return `
+        <button class="btn btn-soft ${color} rounded-full">
+          <span>${icon}</span> ${label}
+        </button>
+      `;
+    }).join("");
+
+    const card = document.createElement("div");
+
+    card.innerHTML = `
+      <div class="bg-white rounded-lg shadow-lg space-y-4 p-4 ${statusColor}">
+          <div class="flex justify-between">
+            <div>
+              <img src="${statusIcon}" alt="">
+            </div>
+            <p class="text-[12px] font-bold ${priorityColor}">
+              ${issue.priority.toUpperCase()}
+            </p>
+          </div>
+
+          <div class="space-y-3">
+            <h2 class="text-sm font-semibold">${issue.title}</h2>
+            <p class="text-gray-500 text-[12px]">${issue.description}</p>
+
+            <div>
+              ${labelsHTML}
+            </div>
+          </div>
+
+          <div class="text-gray-500 text-[12px]">
+            <p>#${issue.id} by ${issue.author}</p>
+            <p>${issue.createdAt}</p>
+          </div>
+        </div>
+    `;
+
+    openContainer.appendChild(card);
+
+  });
+
 }
 const renderAllIssues = (issues) => {
   const issueCount = document.getElementById("issue-count");
